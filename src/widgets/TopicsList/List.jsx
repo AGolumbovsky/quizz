2093c1fb670e7af9c5bbc1f08d9b94b1aee9fb18
Andriy Golumbovsky <agolumbovsky@gmail.com>
2022-05-30 21:05:48 -0400

    tried to make it better but broke json import

--- src/widgets/TopicsList/List.jsx
+++ src/widgets/TopicsList/List.jsx
@@ -1,44 +1,47 @@
 import { Accordion } from '../../shared/ui/Accordion';
-export const TopicsList = () => {
-	const topics = [
-		{
-			title: 'Verbs in Present',
-			description:
-				'A collection of quizzes on the present tense of the verbs BE, DO, and non-modal verbs',
-			quizzes: [
-				{ id: 1, name: 'quiz 1: BE in present' },
-				{ id: 2, name: 'quiz 2: DO in present' },
-			],
-		},
+import { topics } from '../../topics_list_test.json';
 
-		{
-			title: 'Verbs in Past',
-			description:
-				'A collection of quizzes on the past tense of the verbs BE, DO, and non-modal verbs',
-			quizzes: [
-				{ id: 3, name: 'quiz 1: BE in past' },
-				{ id: 4, name: 'quiz 2: DO in past' },
-			],
-		},
-		{
-			title: 'Modal Verbs',
-			description: 'A collection of quizzes on modal verbs and Future tense',
-			quizzes: [
-				{ id: 5, name: 'quiz 1: WILL' },
-				{ id: 6, name: 'quiz 2: CAN' },
-				{ id: 7, name: 'quiz 3: SHOULD' },
-			],
-		},
-		{
-			title: 'Negations',
-			description: 'A collection of quizzes on negations with NOT and other elements',
-			quizzes: [
-				{ id: 8, name: 'quiz 1: NOT' },
-				{ id: 9, name: 'quiz 2: NEVER, NOBODY, NO, etc' },
-				{ id: 10, name: 'quiz 3: no double negation' },
-			],
-		},
-	];
+export const TopicsList = () => {
+	console.log(topics);
+	// const topics = [
+	// 	{
+	// 		title: 'Verbs in Present',
+	// 		description:
+	// 			'A collection of quizzes on the present tense of the verbs BE, DO, and non-modal verbs',
+	// 		quizzes: [
+	// 			{ id: 1, name: 'quiz 1: BE in present' },
+	// 			{ id: 2, name: 'quiz 2: DO in present' },
+	// 		],
+	// 	},
+
+	// 	{
+	// 		title: 'Verbs in Past',
+	// 		description:
+	// 			'A collection of quizzes on the past tense of the verbs BE, DO, and non-modal verbs',
+	// 		quizzes: [
+	// 			{ id: 3, name: 'quiz 1: BE in past' },
+	// 			{ id: 4, name: 'quiz 2: DO in past' },
+	// 		],
+	// 	},
+	// 	{
+	// 		title: 'Modal Verbs',
+	// 		description: 'A collection of quizzes on modal verbs and Future tense',
+	// 		quizzes: [
+	// 			{ id: 5, name: 'quiz 1: WILL' },
+	// 			{ id: 6, name: 'quiz 2: CAN' },
+	// 			{ id: 7, name: 'quiz 3: SHOULD' },
+	// 		],
+	// 	},
+	// 	{
+	// 		title: 'Negations',
+	// 		description: 'A collection of quizzes on negations with NOT and other elements',
+	// 		quizzes: [
+	// 			{ id: 8, name: 'quiz 1: NOT' },
+	// 			{ id: 9, name: 'quiz 2: NEVER, NOBODY, NO, etc' },
+	// 			{ id: 10, name: 'quiz 3: no double negation' },
+	// 		],
+	// 	},
+	// ];
 
 	return (
 		<div>
