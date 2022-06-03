697df1bfd6db0357c3a8a0a7c9b6f00a13b0b553
Andriy Golumbovsky <agolumbovsky@gmail.com>
2022-06-02 21:15:11 -0400

    unbreak my heart, also unbreak my code...

---
+++ src/features/Quiz/ui/StartScreen.js
@@ -0,0 +1,13 @@
+// import { useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { Quiz } from '../features/Quiz';
+// import { api } from '../shared/api';
+
+// export const StartScreen = () => {
+// 	return (
+// 		<div>
+// 			ass
+// 			<Quiz />
+// 		</div>
+// 	);
+// };
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Quiz } from '../features/Quiz';
+import { StartScreen } from '../features/Quiz/ui/StartScreen';
 import { api } from '../shared/api';
 
 export const QuizPage = () => {
