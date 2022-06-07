f5ace49424880b815a8424f3d579a1bff32ea95a
Andriy Golumbovsky <agolumbovsky@gmail.com>
2022-06-07 19:16:55 -0400

    catch up

--- src/features/Quiz/ui/CurrentQuestion.js
+++ src/features/Quiz/ui/CurrentQuestion.js
@@ -1,12 +1,17 @@
 import { Typography } from '@mui/material';
 
-export const CurrentQuestion = () => {
+export const CurrentQuestion = (props) => {
 	const questions = ['Question 1', 'Question 2', 'Question 3'];
 	const listQuestions = questions.map((question) => (
 		<Typography key={question}>{question}</Typography>
 	));
 	const onNext = () => {
+		console.log('now what?');
 		return <div>Now What?</div>;
+	};
+	const onPrev = () => {
+		console.log("let's go back");
+		return <div>Back to square one </div>;
 	};
 	return (
 		<div>
@@ -14,9 +19,9 @@ export const CurrentQuestion = () => {
 			{listQuestions}
 			<Typography>question What</Typography>
 			<Typography>question Yeah</Typography>
-			<button>Previous</button>
+			<button onClick={onPrev}>Previous</button>
 			<button>Answer</button>
-			<button>Next</button>
+			<button onClick={onNext}>Next</button>
 		</div>
 	);
 };
